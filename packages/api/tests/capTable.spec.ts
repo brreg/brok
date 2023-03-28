ca05b174e3c9c91cef4d41a9cb58dec11e452829
Øyvind Hatland <oha@brreg.no>
2023-03-28 13:05:17 +0200

    uncomment test

--- packages/api/tests/capTable.spec.ts
+++ packages/api/tests/capTable.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+import { faker } from '@faker-js/faker';
 import { ethers } from "ethers";
 import { CapTable, CapTableRegistry__factory, CapTable__factory } from "@brok/captable";
 import { GET_PROVIDER, WALLET } from "../src/contants";
@@ -6,26 +7,24 @@ import { GET_PROVIDER, WALLET } from "../src/contants";
 // import debug from 'debug'
 // const log = debug("brok:test:capTable")
 // test.beforeAll(() => {
-
+	
 // });
 
 test("/api/capTable should return deploy and registry transaction hash", async ({ request, baseURL }) => {
+
 	// orgnr
 	const min = Math.ceil(11111111);
-	const max = Math.floor(999999999);
-	const orgnr = Math.floor(Math.random() * (max - min + 1) + min);
+  const max = Math.floor(999999999);
+  const orgnr = Math.floor(Math.random() * (max - min + 1) + min);
 
 	// log(orgnr)
 	const res = await request.post(`${baseURL}/api/capTable`, {
 		headers: {
 			"Content-Type": "application/json",
 		},
-		data: JSON.stringify({ name: "Playwrigth", orgnr: orgnr }),
+		data: JSON.stringify({ name: faker.company.name(), orgnr: orgnr}),
 	});
 	const json = await res.json();
-	if (!res.ok()) {
-		console.log("json:", json);
-	}
 	expect(res.ok(), json).toBe(true);
 
 	expect(json, "json object should be defined").toBeDefined();
@@ -40,6 +39,14 @@ test("/api/capTable should return deploy and registry transaction hash", async (
 		"capTableRegistryTransactionHash" in json,
 		"json object should have property capTableRegistryTransactionHash",
 	).toBe(true);
+
+	expect(json, "json object should be defined").toBeDefined();
+	expect(typeof json).toBe("object");
+	expect(Object.keys(json).length, "json should have properties" + JSON.stringify(json)).toBeGreaterThan(0)
+	expect("capTableAddress" in json, "json object should have property capTableAddress").toBe(true);
+	expect("capTableDeployTransactionHash" in json, "json object should have property capTableDeployTransactionHash").toBe(true);
+	expect("capTableRegistryTransactionHash" in json, "json object should have property capTableRegistryTransactionHash").toBe(true);
+
 
 	// const wallet = WALLET.connect(GET_PROVIDER())
 	// console.log("wallet:", wallet)
