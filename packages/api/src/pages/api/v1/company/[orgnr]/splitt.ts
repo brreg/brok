cee6de232db85d7e09a92d38c278a6f88a2a8ae2
Jon Ramvi <ramvi@users.noreply.github.com>
2023-09-08 13:27:28 +0200

    Startet på splitt og spleis. Begynt å gjøre om til at fnr brukes istedenfor adr + cleanup

--- packages/api/src/pages/api/v1/company/[orgnr]/splitt.ts
+++ packages/api/src/pages/api/v1/company/[orgnr]/splitt.ts
@@ -1,77 +1,3 @@
-/**
-# API Documentation for Share Issuance Service
-
-## Overview
-
-This API is responsible for issuing new shares in a company's capital table. It allows you to specify the organizational number (orgnr), share classes (aksjeklasser), recipient addresses (mottakerAdresser), and the number of shares (antall) to be issued for each recipient.
-
-## API Endpoint
-
-**Method**: POST  
-**Endpoint**: The specific URL for the API will be where this code is deployed. The `orgnr` should be passed as a query parameter.
-
-### Request Parameters
-
-1. **orgnr** (Query Parameter)
-	- Type: String
-	- Description: The organizational number of the company.
-
-2. **Body**
-
-	- **aksjeklasser**
-		- Type: Array of Strings
-		- Description: Specifies the classes/types of shares being issued.
-		- Example: `["Class A", "Class B"]`
-	    
-	- **mottakerAdresser**
-		- Type: Array of Strings
-		- Description: Ethereum addresses of the recipients to whom the new shares are to be issued.
-		- Example: `["0xabc...", "0xdef..."]`
-	    
-	- **antall**
-		- Type: Array of Strings
-		- Description: Specifies the number of shares to issue for each recipient address. The number of elements must match the `mottakerAdresser` array.
-		- Example: `["10", "20"]`
-
-### Request Example
-
-```json
-POST /v1/company/123456789/kapitalforhoyelse
-{
-	"aksjeklasser": ["Class A", "Class B"],
-	"mottakerAdresser": ["0xabc...", "0xdef..."],
-	"antall": ["10", "20"]
-}
-```
-
-### Response Format
-
-- HTTP Status Code: 200 OK on success, error codes on failure.
-- Response Body: JSON object containing the message and details about the transaction.
-
-#### Response Example (Success)
-
-```json
-{
-	"message": "Successfully issued 30 new shares to 2 addresses"
-}
-```
-
-## Errors
-
-- **404 Not Found**: Could not find any company with the provided `orgnr`.
-- **400 Bad Request**: If required fields are missing or invalid.
-
-### Example (Error)
-
-```json
-{
-	"message": "Could not find any company with orgnr 123456789 in BRØK",
-	"statusCode": 404
-}
-```
- */
-
 import { CapTable } from "@brok/captable";
 import debug from "debug";
 import { ethers } from "ethers";
@@ -81,27 +7,18 @@ import { ApiRequestLogger, ErrorResponse } from "../../../../../utils/api";
 import { ConnectToCapTableRegistry_R, ConnectToCapTable_R } from "../../../../../utils/blockchain";
 import { GET_PROVIDER, WALLET } from "../../../../../contants";
 
-const log = debug("brok:api:v1:company:[id]:kapitalforhoyelse");
+const log = debug("brok:api:v1:company:[id]/splitt");
 type Data = {};
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
 	try {
 		ApiRequestLogger(req, log);
 
-		// TODO Hvis aksjeklassen ikke finnes: fail. Eget ende-punkt for å opprette aksjeklasse
-		// Create account for hver bruker
 		switch (req.method) {
 			case "POST": {
 				// Find info about company
 				const { orgnr } = parseQuery(req.query);
-				const { aksjeklasser, mottakere, antall } = req.body;
-
-
-				// SEND LISTE OVER ADR TIL FNR[] + ORGNR, GET WALLET ADDRESS for fnr
-
-
-				// CREATE WALLET FOR HVER BRUKER I LISTEN
-				// og så send inn
+				const { aksjeklasser, mottakerAdresser, antall } = req.body;
 
 				const captable = await findCapTableWithOrgnr(orgnr);
 
@@ -116,7 +33,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 				const partitions = aksjeklasser.map((klass: string) => ethers.utils.formatBytes32String(klass));
 
 				// Issue
-				await captable_RW.kapitalforhoyselse_nye_aksjer(partitions, mottakerAdresser, antall, "0x11");
+				await captable_RW.splitt(partitions, mottakerAdresser, antall, "0x11");
 
 				const sum: number = antall.reduce(
 					(accumulator: number, currentValue: string) => accumulator + parseInt(currentValue, 10),
