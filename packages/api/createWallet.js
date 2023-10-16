c20adfc89c14a55642779dd02938f180777304db
Øyvind Hatland <oha@brreg.no>
2023-10-16 10:11:57 +0200

    bugfix in navnetjener and explain purpose of createWallet values

--- packages/api/createWallet.js
+++ packages/api/createWallet.js
@@ -2,9 +2,9 @@ const ethers = require('ethers');
 
 function createWallet() {
     const wallet = ethers.Wallet.createRandom();
-    console.log('Wallet Address:', wallet.address);
-    console.log('Private Key:', wallet.privateKey);
-    console.log('Mnemonic Phrase:', wallet.mnemonic.phrase);
+    console.log('Wallet Address:', wallet.address, 'Use this as DEV_ENTERPRISE_SYSTEM_ADDRESS in packages/captable/.env');
+    console.log('Private Key:', wallet.privateKey, 'Use this as PRIVATE_KEY in packages/api/.env');
+    console.log('Mnemonic Phrase:', wallet.mnemonic.phrase, 'Use this as SEED_DEV in packages/captable/.env');
 }
 
 createWallet();
