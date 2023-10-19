241ca3b352eb17b91bbd90cf3457a32363089119
Robertosnap <robertosnap@pm.me>
2023-10-19 09:31:46 +0200

    Change graph network to sepolia

--- packages/graph/scripts/prepare.js
+++ packages/graph/scripts/prepare.js
@@ -1,48 +1,47 @@
-const brok = require('@brok/captable');
-const yaml = require('js-yaml');
-const fs = require('fs');
+const brok = require("@brok/captable");
+const yaml = require("js-yaml");
+const fs = require("fs");
 
 const args = process.argv.slice(2);
 const env = args[0];
-const subgraphConfig = yaml.load(fs.readFileSync('subgraph.example.yaml', 'utf8'));
+const subgraphConfig = yaml.load(fs.readFileSync("subgraph.example.yaml", "utf8"));
 
 // set address
 for (const dataSource of subgraphConfig.dataSources) {
-    dataSource.source.address = brok[`${env}Contracts`].CAP_TABLE_REGISTRY;
-    console.log("use this address for CapTableRegistry", dataSource.source.address)
-    if(env === 'localhost') {
-        dataSource.source.startBlock =  1;
-    } 
-    if (env === 'brokStage' || env === 'brokDev') {
-        dataSource.source.startBlock =  
-        15853316;
-    } 
-    if( env === 'brokProd') {
-        dataSource.source.startBlock =  15853316;
-    }
+	dataSource.source.address = brok[`${env}Contracts`].CAP_TABLE_REGISTRY;
+	console.log("use this address for CapTableRegistry", dataSource.source.address);
+	if (env === "localhost") {
+		dataSource.source.startBlock = 1;
+	}
+	if (env === "brokStage" || env === "brokDev") {
+		dataSource.source.startBlock = 628100;
+	}
+	if (env === "brokProd") {
+		dataSource.source.startBlock = 15853316;
+	}
 }
 // set chain
-if(env === 'localhost'){
-    for (const dataSource of subgraphConfig.dataSources) {
-        dataSource.network = "mainnet";
-    }
-    for (const template of subgraphConfig.templates) {
-        template.network = "mainnet";
-    }
-} else if (env === 'brokProd'){
-    for (const dataSource of subgraphConfig.dataSources) {
-        dataSource.network = "mainnet";
-    }
-    for (const template of subgraphConfig.templates) {
-        template.network = "mainnet";
-    }
-}else{
-    for (const dataSource of subgraphConfig.dataSources) {
-        dataSource.network = "arbitrum-goerli";
-    }
-    for (const template of subgraphConfig.templates) {
-        template.network = "arbitrum-goerli";
-    }
+if (env === "localhost") {
+	for (const dataSource of subgraphConfig.dataSources) {
+		dataSource.network = "mainnet";
+	}
+	for (const template of subgraphConfig.templates) {
+		template.network = "mainnet";
+	}
+} else if (env === "brokProd") {
+	for (const dataSource of subgraphConfig.dataSources) {
+		dataSource.network = "mainnet";
+	}
+	for (const template of subgraphConfig.templates) {
+		template.network = "mainnet";
+	}
+} else {
+	for (const dataSource of subgraphConfig.dataSources) {
+		dataSource.network = "arbitrum-sepolia";
+	}
+	for (const template of subgraphConfig.templates) {
+		template.network = "arbitrum-sepolia";
+	}
 }
 
-fs.writeFileSync('subgraph.yaml', yaml.dump(subgraphConfig));
+fs.writeFileSync("subgraph.yaml", yaml.dump(subgraphConfig));
