92a52222431ac8459388de0e65ae607dc8d69f67
Robertosnap <robertosnap@pm.me>
2023-10-25 10:14:56 +0200

    Formatting

--- packages/api/src/pages/api/v1/company/index.ts
+++ packages/api/src/pages/api/v1/company/index.ts
@@ -54,12 +54,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 	try {
 		ApiRequestLogger(req, log);
 
-		res.setHeader('Access-Control-Allow-Origin', '*');
-		res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-		res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+		res.setHeader("Access-Control-Allow-Origin", "*");
+		res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+		res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-
-		if (req.method === 'OPTIONS') {
+		if (req.method === "OPTIONS") {
 			return res.status(200).end();
 		}
 
